424c2ae1ff1e8d4f0c2972ff19e60d94b057acce
Aryanbharti214 <bhartiaryan214@gmail.com>
2026-07-19 19:23:29 +0530

    Stage 4: full CRUD

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -43,11 +43,12 @@ function createTask(req,res){
             message:"Kindly provide the title for the task"
         })
     }
-    const task=tasks.push({
+    const task={
         id:tasks.length,
         title:title,
         done:false
-    })
+    }
+    tasks.push(task);
     return res.status(201).json({
         message:"Task Created successfully",
         task:task
@@ -55,4 +56,41 @@ function createTask(req,res){
 
 }
 
-module.exports={getAllTasks,getTaskById,createTask}
+function updateTask(req,res){
+    const id=Number(req.params.id);
+    const {title,done}=req.body;
+    if(title===undefined&&done===undefined){
+        return res.status(400).json({
+            message:"kindly fill one field"
+        })
+    }
+    for(let i=0;i<tasks.length;i++){
+        if(id===tasks[i].id){
+            tasks[i]={
+                id:id,
+                title:title!==undefined?title:tasks[i].title,
+                done:done!==undefined?done:tasks[i].done
+            }
+            return res.status(200).json({
+                message:"task updated successfully",
+                task:tasks[i]
+            })
+        }
+    }
+    return res.status(404).json({
+        message:"Task not found invalid id"
+    })
+}
+
+function deleteTask(req,res){
+    const id=Number(req.params.id);
+    let length=tasks.length;
+    tasks=tasks.filter(task=> task.id!==id);
+    if(length===tasks.length){
+        return res.status(404).json({
+            message:"id Not found"
+        })
+    }
+    return res.status(204).send();
+}
+module.exports={getAllTasks,getTaskById,createTask,updateTask,deleteTask}
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const router=express.Router()
-const {getAllTasks,getTaskById,createTask}=require('../controllers/task.controller')
+const {getAllTasks,getTaskById,createTask,updateTask,deleteTask}=require('../controllers/task.controller')
 router.get('/',(req,res)=>{
     return res.status(200).json({
         "name": "Task API",
@@ -12,6 +12,8 @@ router.get('/',(req,res)=>{
 router.get('/tasks',getAllTasks);
 router.get('/tasks/:id',getTaskById);
 router.post('/tasks',createTask);
+router.put('/tasks/:id',updateTask);
+router.delete('/tasks/:id',deleteTask);
 router.get('/health',(req,res)=>{
     return res.status(200).json({ "status": "ok" });
 })
